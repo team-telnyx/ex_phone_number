668a4f6271df12adaa85989b0d72ec3bb577dddd
josemrb <work@josemrb.com>
2020-10-27 01:35:26 -0400

    Update ValidationTest
    * Update tests to v8.10.23
    * Group tests according to JavaScript source code

--- test/ex_phone_number/validation_test.exs
+++ test/ex_phone_number/validation_test.exs
@@ -27,178 +27,101 @@ defmodule ExPhoneNumber.ValidationTest do
 
   describe ".is_possible_number_with_reason?/1" do
     test "IsPossibleNumberWithReason" do
-      assert ValidationResults.is_possible() ==
-               is_possible_number_with_reason?(PhoneNumberFixture.us_number())
-
-      assert ValidationResults.is_possible() ==
-               is_possible_number_with_reason?(PhoneNumberFixture.us_local_number())
-
-      assert ValidationResults.too_long() ==
-               is_possible_number_with_reason?(PhoneNumberFixture.us_long_number())
-
-      assert ValidationResults.invalid_country_code() ==
-               is_possible_number_with_reason?(PhoneNumberFixture.unknown_country_code3())
-
-      assert ValidationResults.too_short() ==
-               is_possible_number_with_reason?(PhoneNumberFixture.nanpa_short_number())
-
-      assert ValidationResults.is_possible() ==
-               is_possible_number_with_reason?(PhoneNumberFixture.sg_number2())
-
-      assert ValidationResults.too_long() ==
-               is_possible_number_with_reason?(PhoneNumberFixture.international_toll_free_too_long())
+      assert ValidationResults.is_possible() == is_possible_number_with_reason?(PhoneNumberFixture.us_number())
+      assert ValidationResults.is_possible() == is_possible_number_with_reason?(PhoneNumberFixture.us_local_number())
+      assert ValidationResults.too_long() == is_possible_number_with_reason?(PhoneNumberFixture.us_long_number())
+      assert ValidationResults.invalid_country_code() == is_possible_number_with_reason?(PhoneNumberFixture.unknown_country_code3())
+      assert ValidationResults.too_short() == is_possible_number_with_reason?(PhoneNumberFixture.nanpa_short_number())
+      assert ValidationResults.is_possible() == is_possible_number_with_reason?(PhoneNumberFixture.sg_number2())
+      assert ValidationResults.too_long() == is_possible_number_with_reason?(PhoneNumberFixture.international_toll_free_too_long())
     end
   end
 
   describe ".is_viable_phone_number?/1" do
-    test "should contain at least 2 chars" do
+    test "IsViablePhoneNumber" do
       refute is_viable_phone_number?("1")
-    end
-
-    test "should allow only one or two digits before strange non-possible puntuaction ascii" do
       refute is_viable_phone_number?("1+1+1")
       refute is_viable_phone_number?("80+0")
-    end
-
-    test "should allow two or more digits" do
       assert is_viable_phone_number?("00")
       assert is_viable_phone_number?("111")
-    end
-
-    test "should allow alpha numbers" do
       assert is_viable_phone_number?("0800-4-pizza")
       assert is_viable_phone_number?("0800-4-PIZZA")
-    end
-
-    test "should contain at least three digits before any alpha char" do
       refute is_viable_phone_number?("08-PIZZA")
       refute is_viable_phone_number?("8-PIZZA")
       refute is_viable_phone_number?("12. March")
     end
 
-    test "should allow only one or two digits before strange non-possible puntuaction unicode" do
+    test "IsViablePhoneNumberNonAscii" do
       assert is_viable_phone_number?("1\u300034")
       refute is_viable_phone_number?("1\u30003+4")
-    end
-
-    test "should allow unicode variants of starting chars" do
       assert is_viable_phone_number?("\uFF081\uFF09\u30003456789")
-    end
-
-    test "should allow leading plus sign" do
       assert is_viable_phone_number?("+1\uFF09\u30003456789")
     end
   end
 
   describe ".is_valid_number/1" do
-    test "Valid numbers" do
+    test "IsValidNumber" do
       assert is_valid_number?(PhoneNumberFixture.us_number())
-
       assert is_valid_number?(PhoneNumberFixture.it_number())
-
       assert is_valid_number?(PhoneNumberFixture.gb_mobile())
-
       assert is_valid_number?(PhoneNumberFixture.international_toll_free())
-
       assert is_valid_number?(PhoneNumberFixture.universal_premium_rate())
-
       assert is_valid_number?(PhoneNumberFixture.nz_number2())
     end
 
-    test "Invalid numbers" do
+    test "IsValidForRegion" do
+      assert is_valid_number?(PhoneNumberFixture.bs_number())
+      refute is_valid_number?(PhoneNumberFixture.bs_number_invalid())
+      assert is_valid_number?(PhoneNumberFixture.re_number())
+      refute is_valid_number?(PhoneNumberFixture.re_number_invalid())
+    end
+
+    test "IsNotValidNumber" do
       refute is_valid_number?(PhoneNumberFixture.us_local_number())
-
       refute is_valid_number?(PhoneNumberFixture.it_invalid())
-
       refute is_valid_number?(PhoneNumberFixture.gb_invalid())
-
       refute is_valid_number?(PhoneNumberFixture.de_invalid())
-
       refute is_valid_number?(PhoneNumberFixture.nz_invalid())
-
+      refute is_valid_number?(PhoneNumberFixture.unknown_country_code())
+      refute is_valid_number?(PhoneNumberFixture.unknown_country_code2())
       refute is_valid_number?(PhoneNumberFixture.international_toll_free_too_long())
     end
 
-    test "Invalid country code" do
-      refute is_valid_number?(PhoneNumberFixture.unknown_country_code())
-
-      refute is_valid_number?(PhoneNumberFixture.unknown_country_code2())
+    test "CountryWithNoNumberDesc" do
+      refute is_valid_number?(PhoneNumberFixture.ad_number())
     end
 
-    test "BS number" do
-      assert is_valid_number?(PhoneNumberFixture.bs_number())
-
-      refute is_valid_number?(PhoneNumberFixture.bs_number_invalid())
+    test "UnknownCountryCallingCode" do
+      refute is_valid_number?(PhoneNumberFixture.unknown_country_code_no_raw_input())
     end
 
-    test "DE invalid number returns false #2" do
+    test "DE invalid number returns false" do
       {result, phone_number} = ExPhoneNumber.parse("+494915778961257", "DE")
 
       assert :ok == result
       refute is_valid_number?(phone_number)
     end
-
-    test "RE number" do
-      assert is_valid_number?(PhoneNumberFixture.re_number())
-
-      refute is_valid_number?(PhoneNumberFixture.re_number_invalid())
-    end
-
-    test "YT number" do
-      assert is_valid_number?(PhoneNumberFixture.yt_number())
-    end
   end
 
   describe ".is_valid_number_for_region?/2" do
-    test "BS number" do
+    test "IsValidForRegion" do
       assert is_valid_number_for_region?(PhoneNumberFixture.bs_number(), RegionCodeFixture.bs())
-
       refute is_valid_number_for_region?(PhoneNumberFixture.bs_number(), RegionCodeFixture.us())
-    end
-
-    test "RE number" do
       assert is_valid_number_for_region?(PhoneNumberFixture.re_number(), RegionCodeFixture.re())
-
       refute is_valid_number_for_region?(PhoneNumberFixture.re_number(), RegionCodeFixture.yt())
-
-      refute is_valid_number_for_region?(PhoneNumberFixture.re_number_invalid(), RegionCodeFixture.re())
-
-      refute is_valid_number_for_region?(PhoneNumberFixture.re_number_invalid(), RegionCodeFixture.yt())
-    end
-
-    test "YT number" do
       assert is_valid_number_for_region?(PhoneNumberFixture.yt_number(), RegionCodeFixture.yt())
-
       refute is_valid_number_for_region?(PhoneNumberFixture.yt_number(), RegionCodeFixture.re())
-    end
-
-    test "Multi Country number" do
-      assert is_valid_number_for_region?(
-               PhoneNumberFixture.re_yt_number(),
-               RegionCodeFixture.re()
-             )
-
-      assert is_valid_number_for_region?(
-               PhoneNumberFixture.re_yt_number(),
-               RegionCodeFixture.yt()
-             )
-    end
-
-    test "International Toll Free" do
-      assert is_valid_number_for_region?(
-               PhoneNumberFixture.international_toll_free(),
-               RegionCodeFixture.un001()
-             )
-
-      refute is_valid_number_for_region?(
-               PhoneNumberFixture.international_toll_free(),
-               RegionCodeFixture.us()
-             )
-
-      refute is_valid_number_for_region?(
-               PhoneNumberFixture.international_toll_free(),
-               RegionCodeFixture.zz()
-             )
+      refute is_valid_number_for_region?(PhoneNumberFixture.re_number_invalid(), RegionCodeFixture.re())
+      refute is_valid_number_for_region?(PhoneNumberFixture.re_number_invalid(), RegionCodeFixture.yt())
+      assert is_valid_number_for_region?(PhoneNumberFixture.re_yt_number(), RegionCodeFixture.re())
+      assert is_valid_number_for_region?(PhoneNumberFixture.re_yt_number(), RegionCodeFixture.yt())
+      assert is_valid_number_for_region?(PhoneNumberFixture.international_toll_free(), RegionCodeFixture.un001())
+      refute is_valid_number_for_region?(PhoneNumberFixture.international_toll_free(), RegionCodeFixture.us())
+      refute is_valid_number_for_region?(PhoneNumberFixture.international_toll_free(), RegionCodeFixture.zz())
+      refute is_valid_number_for_region?(PhoneNumberFixture.unknown_country_code(), RegionCodeFixture.zz())
+      refute is_valid_number_for_region?(PhoneNumberFixture.unknown_country_code(), RegionCodeFixture.un001())
+      refute is_valid_number_for_region?(PhoneNumberFixture.unknown_country_code2(), RegionCodeFixture.zz())
+      refute is_valid_number_for_region?(PhoneNumberFixture.unknown_country_code2(), RegionCodeFixture.un001())
     end
   end
 
@@ -214,81 +137,61 @@ defmodule ExPhoneNumber.ValidationTest do
   end
 
   describe ".get_number_type/1" do
-    test "Premium Rate" do
-      assert get_number_type(PhoneNumberFixture.us_premium()) == PhoneNumberTypes.premium_rate()
-
-      assert get_number_type(PhoneNumberFixture.it_premium()) == PhoneNumberTypes.premium_rate()
-
-      assert get_number_type(PhoneNumberFixture.gb_premium()) == PhoneNumberTypes.premium_rate()
-
-      assert get_number_type(PhoneNumberFixture.de_premium()) == PhoneNumberTypes.premium_rate()
-
-      assert get_number_type(PhoneNumberFixture.de_premium2()) ==
-               PhoneNumberTypes.premium_rate()
-
-      assert get_number_type(PhoneNumberFixture.universal_premium_rate()) ==
-               PhoneNumberTypes.premium_rate()
+    test "IsPremiumRate" do
+      assert PhoneNumberTypes.premium_rate() == get_number_type(PhoneNumberFixture.us_premium())
+      assert PhoneNumberTypes.premium_rate() == get_number_type(PhoneNumberFixture.it_premium())
+      assert PhoneNumberTypes.premium_rate() == get_number_type(PhoneNumberFixture.gb_premium())
+      assert PhoneNumberTypes.premium_rate() == get_number_type(PhoneNumberFixture.de_premium())
+      assert PhoneNumberTypes.premium_rate() == get_number_type(PhoneNumberFixture.de_premium2())
+      assert PhoneNumberTypes.premium_rate() == get_number_type(PhoneNumberFixture.universal_premium_rate())
     end
 
-    test "Toll Free" do
-      assert get_number_type(PhoneNumberFixture.us_tollfree2()) == PhoneNumberTypes.toll_free()
-
-      assert get_number_type(PhoneNumberFixture.it_toll_free()) == PhoneNumberTypes.toll_free()
-
-      assert get_number_type(PhoneNumberFixture.gb_toll_free()) == PhoneNumberTypes.toll_free()
-
-      assert get_number_type(PhoneNumberFixture.de_toll_free()) == PhoneNumberTypes.toll_free()
-
-      assert get_number_type(PhoneNumberFixture.international_toll_free()) ==
-               PhoneNumberTypes.toll_free()
+    test "IsTollFree" do
+      assert PhoneNumberTypes.toll_free() == get_number_type(PhoneNumberFixture.us_tollfree2())
+      assert PhoneNumberTypes.toll_free() == get_number_type(PhoneNumberFixture.it_toll_free())
+      assert PhoneNumberTypes.toll_free() == get_number_type(PhoneNumberFixture.gb_toll_free())
+      assert PhoneNumberTypes.toll_free() == get_number_type(PhoneNumberFixture.de_toll_free())
+      assert PhoneNumberTypes.toll_free() == get_number_type(PhoneNumberFixture.international_toll_free())
     end
 
-    test "Mobile" do
-      assert get_number_type(PhoneNumberFixture.bs_mobile()) == PhoneNumberTypes.mobile()
-
-      assert get_number_type(PhoneNumberFixture.gb_mobile()) == PhoneNumberTypes.mobile()
-
-      assert get_number_type(PhoneNumberFixture.it_mobile()) == PhoneNumberTypes.mobile()
-
-      assert get_number_type(PhoneNumberFixture.ar_mobile()) == PhoneNumberTypes.mobile()
-
-      assert get_number_type(PhoneNumberFixture.de_mobile()) == PhoneNumberTypes.mobile()
+    test "IsMobile" do
+      assert PhoneNumberTypes.mobile() == get_number_type(PhoneNumberFixture.bs_mobile())
+      assert PhoneNumberTypes.mobile() == get_number_type(PhoneNumberFixture.gb_mobile())
+      assert PhoneNumberTypes.mobile() == get_number_type(PhoneNumberFixture.it_mobile())
+      assert PhoneNumberTypes.mobile() == get_number_type(PhoneNumberFixture.ar_mobile())
+      assert PhoneNumberTypes.mobile() == get_number_type(PhoneNumberFixture.de_mobile())
     end
 
-    test "Fixed Line" do
-      assert get_number_type(PhoneNumberFixture.bs_number()) == PhoneNumberTypes.fixed_line()
-
-      assert get_number_type(PhoneNumberFixture.it_number()) == PhoneNumberTypes.fixed_line()
-
-      assert get_number_type(PhoneNumberFixture.gb_number()) == PhoneNumberTypes.fixed_line()
-
-      assert get_number_type(PhoneNumberFixture.de_number()) == PhoneNumberTypes.fixed_line()
+    test "IsFixedLine" do
+      assert PhoneNumberTypes.fixed_line() == get_number_type(PhoneNumberFixture.bs_number())
+      assert PhoneNumberTypes.fixed_line() == get_number_type(PhoneNumberFixture.it_number())
+      assert PhoneNumberTypes.fixed_line() == get_number_type(PhoneNumberFixture.gb_number())
+      assert PhoneNumberTypes.fixed_line() == get_number_type(PhoneNumberFixture.de_number())
     end
 
-    test "Fixed Line Or Mobile" do
-      assert get_number_type(PhoneNumberFixture.us_number()) ==
-               PhoneNumberTypes.fixed_line_or_mobile()
-
-      assert get_number_type(PhoneNumberFixture.ar_number2()) ==
-               PhoneNumberTypes.fixed_line_or_mobile()
+    test "IsFixedLineAndMobile" do
+      assert PhoneNumberTypes.fixed_line_or_mobile() == get_number_type(PhoneNumberFixture.us_number())
+      assert PhoneNumberTypes.fixed_line_or_mobile() == get_number_type(PhoneNumberFixture.ar_number2())
     end
 
-    test "Shared Cost" do
-      assert get_number_type(PhoneNumberFixture.gb_shared_cost()) ==
-               PhoneNumberTypes.shared_cost()
+    test "IsSharedCost" do
+      assert PhoneNumberTypes.shared_cost() == get_number_type(PhoneNumberFixture.gb_shared_cost())
     end
 
-    test "VOIP" do
-      assert get_number_type(PhoneNumberFixture.gb_voip()) == PhoneNumberTypes.voip()
+    test "IsVoip" do
+      assert PhoneNumberTypes.voip() == get_number_type(PhoneNumberFixture.gb_voip())
     end
 
-    test "Personal Number" do
-      assert get_number_type(PhoneNumberFixture.gb_personal_number()) ==
-               PhoneNumberTypes.personal_number()
+    test "IsPersonalNumber" do
+      assert PhoneNumberTypes.personal_number() == get_number_type(PhoneNumberFixture.gb_personal_number())
     end
 
-    test "UNKNOWN" do
-      assert get_number_type(PhoneNumberFixture.us_local_number()) == PhoneNumberTypes.unknown()
+    test "IsUnknown" do
+      assert PhoneNumberTypes.unknown() == get_number_type(PhoneNumberFixture.us_local_number())
+    end
+
+    test "CountryWithNoNumberDesc" do
+      assert PhoneNumberTypes.unknown() == get_number_type(PhoneNumberFixture.ad_number())
     end
   end
 
@@ -300,7 +203,9 @@ defmodule ExPhoneNumber.ValidationTest do
 
     test "length larger than Constants.Value.max_input_string_length returns {:error, message}" do
       subject =
-        "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890x"
+        "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890" <>
+          "12345678901234567890123456789012345678901234567890123456789012345678901234567890" <>
+          "12345678901234567890123456789012345678901234567890123456789012345678901234567890x"
 
       assert {:error, _} = validate_length(subject)
     end
